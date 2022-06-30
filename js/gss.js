c53590bac81930efd004c8e54511f08e7519bfbb
Galin Iliev <galin.iliev@tick42.com>
2022-06-30 11:56:02 +0300

    G4E-5104: Add Enable Notifications functionality to toolbar. Used both settings.js and Prefs API. Formatting update.

--- js/gss.js
+++ js/gss.js
@@ -1,15 +1,18 @@
-import {gluePromise} from './glue-related.js';
+import { gluePromise } from './glue-related.js';
 
 let gssPromise;
 let currentEntityTypes = [];
 
 init();
+
 function init() {
   gssPromise = new Promise(async (res, rej) => {
     let glue = await gluePromise;
     let gssInstance = new gss.GlueSearchService(glue.agm);
+
     window['gssInstance'] = gssInstance;
-    gssInstance.ready()
+    gssInstance
+      .ready()
       .then(() => {
         console.debug('GSS ready');
         gssInstance.onEntityTypes((err, entityTypes) => {
@@ -19,7 +22,7 @@ function init() {
       })
       .catch(err => {
         console.log('GSS initialization failed', err);
-      })
+      });
   });
 }
 
@@ -27,7 +30,4 @@ function getCurrentEntityTypes() {
   return currentEntityTypes;
 }
 
-export {
-  gssPromise,
-  getCurrentEntityTypes
-}
+export { gssPromise, getCurrentEntityTypes };
--- js/settings.js
+++ js/settings.js
@@ -1,24 +1,30 @@
+import { gluePromise } from './glue-related.js';
 
 let defaultSettings = {
   showHiddenApps: false,
   showTutorial: true,
+  saveDefaultLayout: false,
   searchClients: true,
   searchInstruments: true,
-  saveDefaultLayout: false
-}
+  enableNotifications: true,
+  enableToasts: true
+};
 
 let localStorageSettings;
 
 try {
   localStorageSettings = JSON.parse(localStorage.getItem('toolbar-settings'));
-} catch(er) {
+} catch (er) {
   localStorageSettings = {};
 }
 
 let settings = Object.assign(defaultSettings, localStorageSettings);
 
 init();
-function init() {
+
+async function init() {
+  const glue = await gluePromise;
+
   setInitialSettings();
   populateSettings();
   trackSettingsChange();
@@ -30,27 +36,50 @@ function setInitialSettings() {
 
 function populateSettings() {
   Object.keys(settings).forEach(settingName => {
-    if ((typeof settings[settingName] === 'boolean') && q(`#settings-content [setting="${settingName}"]`)) {
-      let checkbox = q(`#settings-content [setting="${settingName}"]`);
-      getSetting(settingName) ? checkbox.setAttribute('checked', true) : checkbox.removeAttribute('checked');
+    if (
+      typeof settings[settingName] === 'boolean' &&
+      q(`#settings-content [setting='${settingName}']`)
+    ) {
+      let checkbox = q(`#settings-content [setting='${settingName}']`);
+
+      getSetting(settingName)
+        ? checkbox.setAttribute('checked', true)
+        : checkbox.removeAttribute('checked');
     }
   });
-
 }
 
 function trackSettingsChange() {
-  q('#settings-content ').addEventListener('change', (e) => {
-    let settingElement = e.path.find(e => e && e.getAttribute && e.getAttribute('setting'));
+  const settings = getSettings();
+  const changedSetting = {};
+
+  q('#settings-content').addEventListener('change', e => {
+    let settingElement = e.path.find(
+      e => e && e.getAttribute && e.getAttribute('setting')
+    );
+
     if (settingElement) {
-      setSetting(settingElement.getAttribute('setting'), e.srcElement.checked);
+      setSetting(
+        settingElement.getAttribute('setting'),
+        e.srcElement.checked
+      );
     }
+
+    changedSetting[e.target.getAttribute('setting')] =
+      settings[e.target.getAttribute('setting')];
+
+    updateAppPrefs(changedSetting);
   });
 }
 
+async function updateAppPrefs(setting) {
+  await glue.prefs.update({ ...setting });
+}
+
 function setSetting(settingName, value) {
-  console.info('set setting', settingName, value);
   settings[settingName] = value;
   saveSettings();
+  console.info('set setting', settingName, value);
 }
 
 function getSetting(settingName) {
@@ -65,8 +94,4 @@ function saveSettings() {
   localStorage.setItem('toolbar-settings', JSON.stringify(settings));
 }
 
-export {
-  setSetting,
-  getSetting,
-  getSettings
-}
+export { setSetting, getSetting, getSettings };
